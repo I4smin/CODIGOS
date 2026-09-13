bfbf9be753b0f1b29e9f7db2a33f5471c468d8e7
Iasmin <sousaiasmin864@gmail.com>
2026-09-13 16:36:53 -0300

    Create cadastrodenotas.py

---
+++ cadastrodenotas.py
@@ -0,0 +1,30 @@
+notas = []
+contador = 1
+
+while contador <= 10:
+    nota = float(input(f"Digite a {contador}ª nota: "))
+    notas.append(nota)
+    contador += 1
+
+# Calculando os resultados
+maior = max(notas)
+menor = min(notas)
+media = sum(notas) / len(notas)
+
+print("\n--- RESULTADO DA TURMA ---")
+print("Maior nota:", maior)
+print("Menor nota:", menor)
+print("Média da turma:", media)
+
+# Verificando aprovação e reprovação
+aprovados = 0
+reprovados = 0
+
+for nota in notas:
+    if nota > 7:
+        aprovados += 1
+    elif nota < 6.9:
+        reprovados += 1
+
+print("Aprovados:", aprovados)
+print("Reprovados:", reprovados)
